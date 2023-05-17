8b3d28f5139e2f1d361e34dcf0083bc5ae99d372
julienKeyvalue <julien@keyvalue.systems>
2023-05-17 10:39:42 +0530

    Initial Commit

---
+++ ExampleFramework17May/ExampleFramework17May.h
@@ -0,0 +1,18 @@
+//
+//  ExampleFramework17May.h
+//  ExampleFramework17May
+//
+//  Created by Julien on 17/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExampleFramework17May.
+FOUNDATION_EXPORT double ExampleFramework17MayVersionNumber;
+
+//! Project version string for ExampleFramework17May.
+FOUNDATION_EXPORT const unsigned char ExampleFramework17MayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExampleFramework17May/PublicHeader.h>
+
+
